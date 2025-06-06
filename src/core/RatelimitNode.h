23c09d8ae09cd2600881c50c01a5d6396a897500
Joshua <amethystguitaristj@gmail.com>
2025-06-06 23:46:59 +0100

    added  a synchronous way to do rate limiting as well as the multithreaded option

--- src/core/RatelimitNode.h
+++ src/core/RatelimitNode.h
@@ -18,13 +18,13 @@ public:
 
 	void registernode(const std::string& name, const std::string& attributes, std::string& content);
 
-	bool startCheck();
-
 	bool beginLimit();
 
 	void addNewIpaddress(const std::string& ip_address);
 
 	void removeIpaddress(std::string& ip_address);
+
+	void StartSynchronousLimiting()
 
 	bool isRateLimited(const std::string& ip_address);
 
@@ -38,7 +38,7 @@ private:
 
 	std::mutex attempts_mutex;
 
-	std::map<std::string, unsigned short int> ip_attempts_map;
+	std::map<std::string, std::map<uint16_t,uint64_t>> ip_attempts_map;
 };
 
 #endif // !RATELIMITNODE_H
